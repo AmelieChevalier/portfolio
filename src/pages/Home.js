bfc808f9ebf775c7b14ac5c30a8e449a3c9d8c67
Amélie <ameliechevalier3006@gmail.com>
2020-07-22 16:37:44 +0200

    transition on arrow do show project or presentation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import arrowUp from '../images/arrow-up.png';
-import arrowDown from '../images/arrow-down.png';
 import '../styles/Home.css';
 
 function Home() {
 
-  const [presentationIsOpen, setPresentationIsOpen] = useState(false);
   const [projectIsOpen, setProjectIsOpen] = useState(false);
+  const [presentationIsOpen, setPresentationIsOpen] = useState(false);
+
+  const handleOpenPresentation = () => {
+    setPresentationIsOpen(!presentationIsOpen)
+  }
+
+  const handleOpenProject = () => {
+    setProjectIsOpen(!projectIsOpen)
+  }
 
   return (
     <>
@@ -15,13 +22,13 @@ function Home() {
         <h2>Developpeuse Web</h2>
         <h2>ReactJS | NodeJS</h2>
       </div>
-      <div className='personnal-presentation'>
-        <h1>Qui suis-je ?</h1>
-        <span className='open-presentation' style={{ backgroundImage: !presentationIsOpen ? `url(${arrowDown})` :  `url(${arrowUp})` }} />
-      </div>
-      <div className='projects-presentation'>
+      <div className='projects-presentation' onClick={handleOpenProject}>
         <h1>Mes projets</h1>
-        <span className='open-project' style={{ backgroundImage: !projectIsOpen ? `url(${arrowDown})` :  `url(${arrowUp})` }} />
+        <span className={projectIsOpen ? 'project-arrow open-project' : 'project-arrow'} style={{ backgroundImage: `url(${arrowUp})` }} />
+      </div>
+      <div className='personnal-presentation' onClick={handleOpenPresentation}>
+        <h1>Qui suis-je ?</h1>
+        <span className={presentationIsOpen ? 'presentation-arrow open-presentation' : 'presentation-arrow'} style={{ backgroundImage: `url(${arrowUp})` }} />
       </div>
     </>
   )
