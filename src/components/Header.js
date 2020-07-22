05705679c61e135e0e74e663c62fd881f3985677
Amélie <ameliechevalier3006@gmail.com>
2020-07-22 16:14:11 +0200

    home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import adminImage from '../images/admin.png';
 import '../styles/Header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header () {
   return (
-    <div className='header'>
-      <div className='picto-admin' style={{backgroundImage: `url(${adminImage})`}}/>
+    <div className='header-container'>
+      <Link to='/dashboard' className='header'>
+        <div className='picto-admin' style={{backgroundImage: `url(${adminImage})`}}/>
+      </Link>
     </div>
   ) 
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import arrowUp from '../images/arrow-up.png';
+import arrowDown from '../images/arrow-down.png';
+import '../styles/Home.css';
 
 function Home() {
+
+  const [presentationIsOpen, setPresentationIsOpen] = useState(false);
+  const [projectIsOpen, setProjectIsOpen] = useState(false);
+
   return (
-    <div>Bonjour</div>
+    <>
+      <div className='presentation'>
+        <h1>AMELIE CHEVALIER</h1>
+        <h2>Developpeuse Web</h2>
+        <h2>ReactJS | NodeJS</h2>
+      </div>
+      <div className='personnal-presentation'>
+        <h1>Qui suis-je ?</h1>
+        <span className='open-presentation' style={{ backgroundImage: !presentationIsOpen ? `url(${arrowDown})` :  `url(${arrowUp})` }} />
+      </div>
+      <div className='projects-presentation'>
+        <h1>Mes projets</h1>
+        <span className='open-project' style={{ backgroundImage: !projectIsOpen ? `url(${arrowDown})` :  `url(${arrowUp})` }} />
+      </div>
+    </>
   )
 }
 
