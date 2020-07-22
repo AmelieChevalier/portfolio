33f915a7b63cad34d3c9aa878c60ea68a00606e2
Amélie <ameliechevalier3006@gmail.com>
2020-07-22 18:54:06 +0200

    add projects to homepage

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 4000;
 
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 process.on('unhandledRejection', error => {
   console.error('unhandledRejection', JSON.stringify(error), error.stack);
